546f798e36f25ac637e699940c124dcadc126978
caichenghao1991 <caichenghao1991@gmail.com>
2021-12-11 16:15:56 -0600

    notes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from flask import request
 import settings
 
 
@@ -10,6 +10,18 @@ app.config.from_object(settings.Dev)
 def login():
     data = {"msg": "Welcome to my site!"}
     return render_template("index.html", **data)
+
+
+@app.route('/project1', methods=['GET', 'POST'])
+def project1():
+    #stock = request.args.get('magic', 'no')
+    stock = ''
+    if request.method == 'POST':
+        stock = request.form.get('stock', '')
+        print(stock)
+        return render_template('project1.html', **locals())
+
+    return render_template('project1.html', **locals())
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True, threaded=True)
